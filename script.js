6b65d82065e579bfd5ecc9d1cd0c8e5c9c478d8f
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-25 00:04:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+	inputs.forEach((input, index) => {
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace' && input.value.length === 0) {
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+});
