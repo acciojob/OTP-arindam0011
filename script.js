20bad66771dd6a41f0d5b812d0ce9ea5f4b3f4db
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-25 00:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-var inputs = Array.from(document.querySelectorAll('.code'));
+document.addEventListener('DOMContentLoaded', (event) => {
+  var inputs = Array.from(document.querySelectorAll('.code'));
 
-inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {
-    if (input.value.length > 0) {
-      if (index < inputs.length - 1) {
-        inputs[index + 1].focus();
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+      if (input.value.length > 0) {
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
       }
-    }
-  });
+    });
 
-  input.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace' && input.value.length === 0) {
-      if (index > 0) {
-        inputs[index - 1].focus();
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Backspace' && input.value.length === 0) {
+        if (index > 0) {
+          inputs[index - 1].focus();
+        }
       }
-    }
+    });
   });
 });
