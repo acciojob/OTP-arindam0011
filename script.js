7d9e1e9b5eca039ff5304f8f742a1ec2b686d1fb
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-01 00:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
- const inputs = document.querySelectorAll('.code');
+document.addEventListener('DOMContentLoaded', (event) => {
+  var inputs = Array.from(document.querySelectorAll('.code'));
 
   inputs.forEach((input, index) => {
     input.addEventListener('input', () => {
@@ -12,10 +12,15 @@
 
     input.addEventListener('keydown', (event) => {
       if (event.key === 'Backspace' && input.value.length === 0) {
-		  index.value=0;
         if (index > 0) {
           inputs[index - 1].focus();
         }
       }
     });
   });
+
+  // Set the focus on the first input field when the page loads
+  if (inputs.length > 0) {
+    inputs[0].focus();
+  }
+});
