90b940bd1679fa080280a0bb9318dde5f2d2b35e
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-01 00:02:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  var inputs = Array.from(document.querySelectorAll('.code'));
+
+ const inputs = document.querySelectorAll('.code');
 
   inputs.forEach((input, index) => {
     input.addEventListener('input', () => {
@@ -12,10 +12,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     input.addEventListener('keydown', (event) => {
       if (event.key === 'Backspace' && input.value.length === 0) {
+		  index.value=0;
         if (index > 0) {
           inputs[index - 1].focus();
         }
       }
     });
   });
-});
